ef4421c662a2181da64669544af875755a102835
Ian Juma <ian.juma@yahoo.com>
2014-11-21 05:50:59 +0300

    users routes

--- models/users.js
+++ models/users.js
@@ -21,7 +21,8 @@ var User = thinky.createModel('Users', {
         enforce_type: 'strict'
     },
     profile_url: {
-        _type: String
+        _type: String,
+        default: "http://www.gravatar.com/avatar/3b3be63a4c2a439b013787725dfce802?d=identicon"
     },
     provider: String
 });
