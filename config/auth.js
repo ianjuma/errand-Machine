4edd9039e091f2ef7ff945bd9a79fcc43712880f
Ian Juma <ian.juma@yahoo.com>
2014-11-21 19:00:27 +0300

    update user

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
 	facebookAuth : {
 		'clientID' 		: '621922847912624',
 		'clientSecret' 	: '85dbd59fae3f893fdfa250e9874cb340',
-		'callbackURL' 	: 'http://localhost:8000/auth/facebook/callback',
+		'callbackURL' 	: 'https://taskwetu.com/auth/facebook/callback',
 		'enableProof'   : false,
 		'profileFields' : ['id', 'name', 'displayName', 'photos', 'emails']
 	},
@@ -11,7 +11,7 @@ module.exports = {
 	googleAuth : {
 		'clientID' 		: '610994249103-jk2e52c868rk2omnk5hegpirpeki4frm.apps.googleusercontent.com',
 		'clientSecret' 	: 'jjN3mRwZpYXTGayZayCynjWJ',
-		'callbackURL' 	: 'http://127.0.0.1:8000/auth/google/callback'
+		'callbackURL' 	: 'https://taskwetu.com/auth/google/callback'
 	},
 
 	passwordSalt: {
