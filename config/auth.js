dc8b100d1ab804e31c69fbf202b0b3e13e0fe7e1
Ian Juma <ian.juma@yahoo.com>
2015-05-08 18:37:43 +0300

    moving to angular

--- config/auth.js
+++ config/auth.js
@@ -1,16 +1,8 @@
 module.exports = {
 
-	facebookAuth : {
-		'clientID' 		: '',
-		'clientSecret' 	: '',
-		'callbackURL' 	: 'http://localhost:8000/auth/facebook/callback',
-		'enableProof'   : false,
-		'profileFields' : ['id', 'name', 'displayName', 'photos', 'emails']
-	},
-
 	googleAuth : {
-		'clientID' 		: '',
-		'clientSecret' 	: '',
+		'clientID' 		: '610994249103-jk2e52c868rk2omnk5hegpirpeki4frm.apps.googleusercontent.com',
+		'clientSecret' 	: 'jjN3mRwZpYXTGayZayCynjWJ',
 		'callbackURL' 	: 'http://127.0.0.1:8000/auth/google/callback'
 	},
 
