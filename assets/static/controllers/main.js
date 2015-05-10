e26a5395d01d360f816e35000763929a15ec1568
Ian Juma <ian.juma@yahoo.com>
2015-05-10 13:04:37 +0300

    remove old controllers

--- assets/static/controllers/main.js
+++
@@ -1,7 +0,0 @@
-angular.module('metricSlice')
-    .controller('MainController', function($scope) {
-        $scope.title = ['0-9', 'A', 'B', 'C', 'D'];
-        $scope.things = ['Action', 'Adventure', 'Animation', 'Children', 'Comedy',
-            'Crime', 'Documentary'];
-        $scope.docTitle = "Not Much here"
-    });
--- assets/static/services/ussd.js
+++
@@ -1,9 +0,0 @@
-angular.module('metricSlice')
-  .factory('USSD', function($http) {
-    return {
-      save: function(User) {
-        return $http.post('/api/user', { username: User.username, metric: User.metric,
-          startDate: User.startDate, endDate: User.endDate, granularity: User.granularity });
-      }
-    };
-  });
